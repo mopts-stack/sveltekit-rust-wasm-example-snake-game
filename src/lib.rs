e468647976ccd8bffcdee82ef26a484d41a7a8b4
Navid Dezashibi <dezashibi@gmail.com>
2024-04-03 15:01:07 +0400

    snake head draw

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,50 @@ pub fn greet(name: &str) {
 extern "C" {
     pub fn alert(s: &str);
 }
+
+// SNAKE GAME IMPLEMENTATION
+const SIZE: usize = 16;
+
+struct SnakeCell(usize);
+
+struct Snake {
+    body: Vec<SnakeCell>,
+}
+
+impl Snake {
+    fn new(spawn_index: usize) -> Self {
+        Self {
+            body: vec![SnakeCell(spawn_index)],
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub struct World {
+    width: usize,
+    snake: Snake,
+}
+
+impl Default for World {
+    fn default() -> Self {
+        Self {
+            width: SIZE,
+            snake: Snake::new(10),
+        }
+    }
+}
+
+#[wasm_bindgen]
+impl World {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn snake_head_idx(&self) -> usize {
+        self.snake.body[0].0
+    }
+}
---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+	import init, { World } from 'wasm-test';
+	import { onMount } from 'svelte';
+
+	const CELL_SIZE = 48;
+
+	let ctx: CanvasRenderingContext2D | null;
+	let canvas: HTMLCanvasElement | null;
+
+	let world: World | null;
+
+	onMount(() => {
+		init().then((_) => {
+			world = World.new();
+
+			canvas = document.getElementById('game-canvas') as HTMLCanvasElement;
+			ctx = canvas.getContext('2d');
+
+			canvas.height = world.width() * CELL_SIZE;
+			canvas.width = world.width() * CELL_SIZE;
+
+			drawWorld();
+
+			drawSnake();
+
+			console.log(world.snake_head_idx());
+		});
+	});
+
+	const drawWorld = () => {
+		if (ctx === null || world === null) return;
+
+		ctx.beginPath();
+
+		for (let x = 0; x < world.width() + 1; ++x) {
+			ctx.moveTo(CELL_SIZE * x, 0);
+			ctx.lineTo(CELL_SIZE * x, world.width() * CELL_SIZE);
+		}
+
+		for (let y = 0; y < world.width() + 1; ++y) {
+			ctx.moveTo(0, CELL_SIZE * y);
+			ctx.lineTo(world.width() * CELL_SIZE, CELL_SIZE * y);
+		}
+
+		ctx.stroke();
+	};
+
+	const drawSnake = () => {
+		if (ctx === null || world === null) return;
+
+		const snakeIdx = world?.snake_head_idx();
+		const col = snakeIdx % world.width();
+		const row = Math.floor(snakeIdx / world.width());
+
+		ctx.beginPath();
+
+		ctx.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+
+		ctx.stroke();
+	};
+</script>
+
+<div class="content-wrapper">
+	<canvas id="game-canvas"></canvas>
+</div>
+
+<style>
+	.content-wrapper {
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+	}
+</style>
