46b556c9bfe201db75b58083ee5f359e7cf4039b
Navid Dezashibi <dezashibi@gmail.com>
2024-04-03 15:42:59 +0400

    refactor game related files to separate typescript file in lib folder

---
+++ web/src/lib/game.ts
@@ -0,0 +1,57 @@
+import { World } from 'wasm-test';
+
+const CELL_SIZE = 48;
+
+export const update = (ctx: CanvasRenderingContext2D | null, canvas: HTMLCanvasElement | null, world: World | null) => {
+    setTimeout(() => {
+        if (ctx === null || world === null || canvas === null) return;
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        world.update();
+
+        draw(ctx, canvas, world);
+
+        // invoke update again before repaint event
+        requestAnimationFrame(() => update(ctx, canvas, world));
+    }, 100);
+}
+
+export const draw = (ctx: CanvasRenderingContext2D | null, canvas: HTMLCanvasElement | null, world: World | null) => {
+    if (ctx === null || world === null || canvas === null) return;
+
+    drawWorld(ctx, world);
+    drawSnake(ctx, world);
+}
+
+const drawWorld = (ctx: CanvasRenderingContext2D | null, world: World | null) => {
+    if (ctx === null || world === null) return;
+
+    ctx.beginPath();
+
+    for (let x = 0; x < world.width() + 1; ++x) {
+        ctx.moveTo(CELL_SIZE * x, 0);
+        ctx.lineTo(CELL_SIZE * x, world.width() * CELL_SIZE);
+    }
+
+    for (let y = 0; y < world.width() + 1; ++y) {
+        ctx.moveTo(0, CELL_SIZE * y);
+        ctx.lineTo(world.width() * CELL_SIZE, CELL_SIZE * y);
+    }
+
+    ctx.stroke();
+};
+
+const drawSnake = (ctx: CanvasRenderingContext2D | null, world: World | null) => {
+    if (ctx === null || world === null) return;
+
+    const snakeIdx = world?.snake_head_idx();
+    const col = snakeIdx % world.width();
+    const row = Math.floor(snakeIdx / world.width());
+
+    ctx.beginPath();
+
+    ctx.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+
+    ctx.stroke();
+};
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
 	import init, { World } from 'wasm-test';
-	import { onMount, onDestroy } from 'svelte';
+	import { onMount } from 'svelte';
+
+	import { update, draw } from '$lib/game';
 
 	const CELL_SIZE = 48;
 
 	let ctx: CanvasRenderingContext2D | null;
 	let canvas: HTMLCanvasElement | null;
-
 	let world: World | null;
-
-	let interval: number;
 
 	onMount(() => {
 		init().then((_) => {
@@ -21,58 +20,12 @@
 			canvas.height = world.width() * CELL_SIZE;
 			canvas.width = world.width() * CELL_SIZE;
 
+			draw(ctx, canvas, world);
+
 			// Update loop
-			interval = setInterval(() => {
-				if (ctx === null || world === null) return;
-
-				ctx!.clearRect(0, 0, canvas!.width, canvas!.height);
-
-				drawWorld();
-				drawSnake();
-
-				world.update();
-
-				console.log('Updating');
-			}, 100);
+			update(ctx, canvas, world);
 		});
 	});
-
-	onDestroy(() => {
-		// cleanup
-		clearInterval(interval);
-	});
-
-	const drawWorld = () => {
-		if (ctx === null || world === null) return;
-
-		ctx.beginPath();
-
-		for (let x = 0; x < world.width() + 1; ++x) {
-			ctx.moveTo(CELL_SIZE * x, 0);
-			ctx.lineTo(CELL_SIZE * x, world.width() * CELL_SIZE);
-		}
-
-		for (let y = 0; y < world.width() + 1; ++y) {
-			ctx.moveTo(0, CELL_SIZE * y);
-			ctx.lineTo(world.width() * CELL_SIZE, CELL_SIZE * y);
-		}
-
-		ctx.stroke();
-	};
-
-	const drawSnake = () => {
-		if (ctx === null || world === null) return;
-
-		const snakeIdx = world?.snake_head_idx();
-		const col = snakeIdx % world.width();
-		const row = Math.floor(snakeIdx / world.width());
-
-		ctx.beginPath();
-
-		ctx.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-
-		ctx.stroke();
-	};
 </script>
 
 <div class="content-wrapper">
